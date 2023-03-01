afb9b29e72cd6d710fc3fbed862a2e80c8013cab
Mendes Sousa <leoprofessionallogytech@outlook.com>
2023-02-28 23:11:55 -0300

    @v1.0.3

--- app/agent/src/main/java/com/user/agent/enumerables/USCities.java
+++ app/agent/src/main/java/com/user/agent/enumerables/USCities.java
@@ -2,21 +2,15 @@ package com.user.agent.enumerables;
 
 public enum USCities {
 
-	//NEW_YORK
 	New_york,
 	
-	//CALIFORNIA
 	Los_angeles,
 	
-	//ILINOIS
 	Chicago,
 
-	//TEXAS
 	Houston,
 
-	//WISCONSIN
 	Milwaukee,
 
-	//VIRGINIA
 	Richmond
 }
