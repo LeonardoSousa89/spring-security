2315e0a05e34b196a68b301e35cf64ef9b2eacfb
leonardoSousa89 <leoprofessionallogytech@outlook.com>
2023-06-17 14:15:48 -0300

    @v1.0.5

--- security/src/main/resources/anotation.sh
+++
@@ -1,9 +0,0 @@
-#artigos e pesquisas:
-
-#https://cursos.alura.com.br/forum/topico-explicacao-das-anotacoes-jointable-247159
-#https://www.alura.com.br/artigos/entendendo-o-lazy-e-o-eager-load-da-jpa?gclid=EAIaIQobChMI4f614OrI_wIVRn9MCh3AEAlcEAAYASAAEgKGIPD_BwE
-#https://pt.stackoverflow.com/questions/88270/qual-a-finalidade-da-interface-serializable
-#https://projectlombok.org/features/
-#https://refactoring.guru/pt-br/design-patterns/builder/java/example
-#https://pt.stackoverflow.com/questions/190504/capturar-nullpointerexception-é-má-prática
-#https://cursos.alura.com.br/forum/topico-java-util-nosuchelementexception-no-value-present-184922
